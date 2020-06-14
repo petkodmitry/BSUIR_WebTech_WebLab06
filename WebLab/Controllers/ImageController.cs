a24144f0e2cd0b48cb5d9925183fbfe2434aacf1
petkodmitry <petkodmitry@gmail.com>
2020-06-14 17:52:55 +0300

    init commit

---
+++ WebLab/Controllers/ImageController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using System.Threading.Tasks;
+using WebLab.DAL.Entities;
+
+namespace WebLab.Controllers
+{
+    public class ImageController : Controller {
+        UserManager<ApplicationUser> _userManager;
+        IHostingEnvironment _env;
+
+        public ImageController(UserManager<ApplicationUser> mngr, IHostingEnvironment env) {
+            _userManager = mngr;
+            _env = env;
+        }
+
+        public async Task<IActionResult> GetAvatar() {
+            var user = await _userManager.GetUserAsync(User);
+            if (user.AvatarImage != null)
+                return File(user.AvatarImage, user.ImageMimeType);
+            else {
+                var avatarPath = "/Images/anonymous.png";
+                var extProvider = new FileExtensionContentTypeProvider();
+                var mimeType = extProvider.Mappings[".png"];
+                return File(_env.WebRootFileProvider.GetFileInfo(avatarPath).CreateReadStream(), mimeType);
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
